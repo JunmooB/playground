306b4d92add956b726a680cb284307b0f8d78fb9
unknown <junmoo.byun@khu.ac.kr>
2024-08-05 20:58:04 +0900

    Moved old_folder to new_folder_2nd

--- playground/00_selenium/main.py
+++
@@ -1,8 +0,0 @@
-import proj_selenium
-
-proj_selenium.test()
-# test = proj_selenium.MacroProgramDLH()
-
-# test.open_page()
-# test.login_to_dlh()
-# test.close_browser()
--- playground/00_selenium/proj_selenium/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import MacroProgramDLH
-
-from .utils import test
